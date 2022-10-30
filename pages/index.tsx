ca2bb6655e1951653b8bb51d3e52b14b25a86d19
Émerson Henrique <emerson.eho@gmail.com>
2022-10-30 04:02:28 +0000

    Correção de implicity data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ data }) {
+export default function Home({ data }: { data: any }) {
   return (
 
     <div className={styles.container}>
