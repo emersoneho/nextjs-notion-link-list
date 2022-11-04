0d94bdeedacf65157107a8a8c16541a15200abbf
Émerson Henrique <emerson.eho@gmail.com>
2022-11-04 13:44:01 +0000

    Remove i18n

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'cf.shopee.com.br'],
   },
-  i18n: {
-    // providing the locales supported by your application
-    locales: ["en-US","pt-BR"],
-    //  default locale used when the non-locale paths are visited
-    defaultLocale: "pt-BR",
-  },
 }
 
 module.exports = nextConfig
